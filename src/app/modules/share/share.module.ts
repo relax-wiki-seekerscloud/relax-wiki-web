8d1b4d4ac32bde827e79c64632bc94579a1e1af3
giharapathi <88247382+giharapathi@users.noreply.github.com>
2023-02-14 17:51:32 +0530

    nav bar component created

--- src/app/modules/share/share.module.ts
+++ src/app/modules/share/share.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ShareRoutingModule } from './share-routing.module';
 import { ShareComponent } from './share.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    ShareComponent
+    ShareComponent,
+    NavBarComponent
+  ],
+  exports: [
+    NavBarComponent
   ],
   imports: [
     CommonModule,
