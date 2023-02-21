d16706c9c8fc006f784bd6b5528a0efd3901fbfe
giharapathi <88247382+giharapathi@users.noreply.github.com>
2023-02-21 21:04:58 +0530

    nav bar two, footer two and relax wiki logo created.

--- src/app/modules/share/share.module.ts
+++ src/app/modules/share/share.module.ts
@@ -5,18 +5,26 @@ import { ShareRoutingModule } from './share-routing.module';
 import { ShareComponent } from './share.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavBarTwoComponent } from './nav-bar-two/nav-bar-two.component';
+import { FooterTwoComponent } from './footer-two/footer-two.component';
+import { RelaxwikiLogoComponent } from './relaxwiki-logo/relaxwiki-logo.component';
 
 
 @NgModule({
   declarations: [
     ShareComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    NavBarTwoComponent,
+    FooterTwoComponent,
+    RelaxwikiLogoComponent
   ],
-    exports: [
-        NavBarComponent,
-        FooterComponent
-    ],
+  exports: [
+    NavBarComponent,
+    FooterComponent,
+    NavBarTwoComponent,
+    FooterTwoComponent
+  ],
   imports: [
     CommonModule,
     ShareRoutingModule
