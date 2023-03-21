af0495ca6685d08ddc1431e75b1acbb11e512518
hasareka <94442124+hasareka@users.noreply.github.com>
2023-03-21 09:23:09 +0530

    feedback,hotel-profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {path:'',redirectTo:'console',pathMatch:'full'},
   { path: 'console', loadChildren: () => import('./modules/console/console.module').then(m => m.ConsoleModule) },
   { path: 'share', loadChildren: () => import('./modules/share/share.module').then(m => m.ShareModule) },
-  { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) }];
+  { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
+  { path: 'hotel-owner-profile', loadChildren: () => import('./modules/console/modules/hotel-management/Modules/hotel-owner-profile/hotel-owner-profile.module').then(m => m.HotelOwnerProfileModule) }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
